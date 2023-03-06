85717a9a0a3b30424b470ed6e913b90e61ed64a0
Samuel Morales <sam@moralessa.dev>
2023-03-06 12:20:41 -0700

    added all button functionality working on travail

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
-import { placeKnight, togglePlaceButton, fillSpace } from "./dom.js";
+import { placeKnight, toggleButton, toggleSpace,  fillSpace } from "./dom.js";
+
 
 class ChessBoard{
     constructor(x = null, y = null){
         this.Xposition = x;
         this.Yposition = y;
+        this.desiredPosition = null;
     }
 
     getMoves(){
         return [-2,-1,1,2];
+    }
+
+    isActive(){
+        if(this.Xposition && this.Yposition) return true;
+        return false;
     }
 
     outOfBounds(desiredMoveArr){
@@ -26,11 +33,14 @@ class ChessBoard{
         return [this.Xposition, this.Yposition];
     }
 
-    knightMoves(currentPositon = [this.Xposition, this.Yposition], desiredPosition, vis){
+    knightMoves(currentPositon = [this.Xposition, this.Yposition], desiredPosition, path){
 
     }
 }
 
+
+let board = new ChessBoard();
+let selectedEnd;
 const placeKnightButton = document.getElementById('place-knight');
 const randomKnightButton = document.getElementById('random-knight');
 const selectEndButton = document.getElementById('select-end');
@@ -38,15 +48,36 @@ const travailButton = document.getElementById('travail');
 const clearButton = document.getElementById('clear');
 const spaces = document.querySelectorAll('.space');
 
-placeKnightButton.addEventListener('click', togglePlaceButton);
+placeKnightButton.addEventListener('click', (e)=>{toggleButton(e.target)});
 
 spaces.forEach(space =>{
     space.addEventListener('click', (e)=>{
         if(placeKnightButton.classList.contains('active')){
-            togglePlaceButton();
+            toggleButton(placeKnightButton);
             fillSpace(e.target);
+            board.Xposition = e.target.getAttribute('data-x');
+            board.Yposition = e.target.getAttribute('data-y');
+        }
+        if(selectEndButton.classList.contains('active') && board.isActive()){
+            toggleSpace(e.target);
+            toggleButton(selectEndButton);
+            let selectedCoordinates = [];
+            selectedCoordinates.push(e.target.getAttribute('data-x'));
+            selectedCoordinates.push(e.target.getAttribute('data-y'));
+            console.log(selectedCoordinates);
+        
         }
     })
 })
 
-placeKnight([2,2]);
+randomKnightButton.addEventListener('click', ()=>{
+    placeKnight(board.randomPosition());
+})
+
+selectEndButton.addEventListener('click', (e)=>{
+    toggleButton(e.target);
+})
+
+clearButton.addEventListener('click', ()=>{
+    location.reload();
+})
--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,29 @@
 const knight = new Image();
 knight.src = '../img/knight-svgrepo-com.svg';
-const placeKnightButton = document.getElementById('place-knight');
 
+function toggleSpace(target){
+    target.classList.toggle('selected');
+}
 
-function togglePlaceButton(){
-    placeKnightButton.classList.toggle('active');
+function toggleButton(targetButton){
+    let buttons = document.querySelectorAll('.active');
+    buttons.forEach(btn =>{
+        if(btn !== targetButton){
+            btn.classList.remove('active');
+        }
+    })
+    targetButton.classList.toggle('active');
 }
 
 function fillSpace(target){
-    console.log('called');
+    console.log(target.classList);
     target.appendChild(knight);
 }
 
 function placeKnight(position){
-    let space = document.querySelector(`#r-${position[1]} > div:nth-child(${position[0] + 1})`);
+    let space = document.querySelector(`div[data-x="${position[0]}"][data-y="${position[1]}"`);
     space.appendChild(knight);
 }
 
 
-export {placeKnight, togglePlaceButton, fillSpace};
+export {placeKnight, toggleButton, toggleSpace, fillSpace};
